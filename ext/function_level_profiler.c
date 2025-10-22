7cd10fd6e64aa470d53312e61103d7fa8afc501d
Jerry Leung <tingfung.leung@solarwinds.com>
2025-10-22 10:59:32 -0700

    Updated optin

--- ext/function_level_profiler.c
+++ ext/function_level_profiler.c
@@ -796,23 +796,26 @@ static function_level_profiler *create_function_level_profiler() {
 }
 
 bool add_function_level_profiler(char *fn, zval *pre_hook, zval *post_hook) {
-    zend_string *lc = zend_string_init(fn, strlen(fn), 0);
-    function_level_profiler *profiler = zend_hash_find_ptr(OTEL_G(function_level_profiler_lookup), lc);
-    if (!profiler) {
-        profiler = create_function_level_profiler();
-        zend_hash_update_ptr(OTEL_G(function_level_profiler_lookup), lc, profiler);
-    }
-    zend_string_release(lc);
+    if (OTEL_G(function_level_profiling)) {
+        zend_string *lc = zend_string_init(fn, strlen(fn), 0);
+        function_level_profiler *profiler = zend_hash_find_ptr(OTEL_G(function_level_profiler_lookup), lc);
+        if (!profiler) {
+            profiler = create_function_level_profiler();
+            zend_hash_update_ptr(OTEL_G(function_level_profiler_lookup), lc, profiler);
+        }
+        zend_string_release(lc);
 
-    if (pre_hook) {
-        zval_add_ref(pre_hook);
-        zend_llist_add_element(&profiler->pre_hooks, pre_hook);
+        if (pre_hook) {
+            zval_add_ref(pre_hook);
+            zend_llist_add_element(&profiler->pre_hooks, pre_hook);
+        }
+        if (post_hook) {
+            zval_add_ref(post_hook);
+            zend_llist_add_element(&profiler->post_hooks, post_hook);
+        }
+        return true;
     }
-    if (post_hook) {
-        zval_add_ref(post_hook);
-        zend_llist_add_element(&profiler->post_hooks, post_hook);
-    }
-    return true;
+    return false;
 }
 
 static void destroy_function_level_profiler_lookup(zval *zv) { free_function_level_profiler(Z_PTR_P(zv)); }
--- ext/tests/function_level_profiler/hook_zend_execute_internal_success_with_optin.phpt
+++ ext/tests/function_level_profiler/hook_zend_execute_internal_success_with_optin.phpt
@@ -2,6 +2,8 @@
 Check if hook_zend_execute_internal returns true
 --EXTENSIONS--
 opentelemetry
+--INI--
+opentelemetry.function_level_profiling=On
 --FILE--
 <?php
 $ret = \OpenTelemetry\Instrumentation\hook_zend_execute_internal();
